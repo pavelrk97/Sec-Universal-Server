a5a04c6e42ac0f31706355f32f7390fe81e5dfdd
pavelrk97 <pavel_r_k97@mail.ru>
2026-05-07 21:56:03 +0300

    AS

---
+++ auth-server-opaque/src/test/java/com/pavelrk97/secserver/authserveropaque/MainTests.java
@@ -0,0 +1,12 @@
+package com.pavelrk97.secserver.authserveropaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
